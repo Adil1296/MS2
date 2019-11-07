85273e6fedbeb63a6461b31a4ee889579212e2c6
Adil1296 <57418112+Adil1296@users.noreply.github.com>
2019-11-08 03:52:08 +0530

    Add files via upload

---
+++ homen/src/main/java/com/example/demo/Usern.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Usern {
+
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private String pssword;
+	private int phone;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPssword() {
+		return pssword;
+	}
+	public void setPssword(String pssword) {
+		this.pssword = pssword;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+}
---
+++ homen/src/main/webapp/Home.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form action="submit" method=post >
+		User Id <input type="text" name="username"/>
+		<br>
+		Password <input type="text" name="password"/>
+		<br>
+		<input type="submit">
+		<br>
+		<br>
+	</form>
+	
+	<form action="Registration" method=post>
+		
+			<button onclick="Login.jsp"> Registration</button>
+		
+	
+	</form>
+
+</body>
+</html>
